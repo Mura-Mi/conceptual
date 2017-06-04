43b8c6179c3cc379c27398bedc11a2394abe3278
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-06-05 07:22:59 +0900

    Add specs for other attributes

--- spec/conceptual/attributes_spec.rb
+++ spec/conceptual/attributes_spec.rb
@@ -14,5 +14,40 @@ RSpec.describe Conceptual::Attribute do
     end
   end
 
+  describe("IntAttribute") do
+    subject(:attr) { Conceptual::IntAttribute.new(:age) }
+
+    describe(:name) do
+      subject { attr.name }
+
+      it "is as initializer receives" do
+        expect(subject).to eq(:age)
+      end
+    end
+  end
+
+  describe("DateAttribute") do
+    subject(:attr) { Conceptual::DateAttribute.new(:birthday) }
+
+    describe(:name) do
+      subject { attr.name }
+
+      it "is as initializer receives" do
+        expect(subject).to eq(:birthday)
+      end
+    end
+  end
+
+  describe("DatetimeAttribute") do
+    subject(:attr) { Conceptual::DateTimeAttribute.new(:created_at) }
+
+    describe(:name) do
+      subject { attr.name }
+
+      it "is as initializer receives" do
+        expect(subject).to eq(:created_at)
+      end
+    end
+  end
 
 end
