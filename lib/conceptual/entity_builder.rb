452493c1afc93c21460464895fee9a6b17fd0985
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-06-05 07:51:05 +0900

    Add usage example spec

--- lib/conceptual/entity_builder.rb
+++ lib/conceptual/entity_builder.rb
@@ -5,23 +5,26 @@ module Conceptual
     attr_reader :attributes
 
     def initialize()
-      @attributes = []
     end
 
     def int(name)
-      @attributes << Conceptual::IntAttribute.new(name)
+      attributes << Conceptual::IntAttribute.new(name)
     end
 
     def string(name)
-      @attributes << Conceptual::StringAttribute.new(name)
+      attributes << Conceptual::StringAttribute.new(name)
     end
 
     def date(name)
-      @attributes << Conceptual::DateAttribute.new(name)
+      attributes << Conceptual::DateAttribute.new(name)
     end
 
     def datetime(name)
-      @attributes << Conceptual::DateTimeAttribute.new(name)
+      attributes << Conceptual::DateTimeAttribute.new(name)
+    end
+
+    def attributes
+      @attributes ||= []
     end
   end
 end
--- spec/conceptual_spec.rb
+++ spec/conceptual_spec.rb
@@ -1,8 +1,53 @@
 require "spec_helper"
 require "conceptual"
+require 'singleton'
+require "conceptual/entity_builder"
+require "conceptual/attributes"
 
 RSpec.describe Conceptual do
   it "has a version number" do
     expect(Conceptual::VERSION).to eq('0.0.1')
   end
+
+  describe "usage" do
+
+    class Player < Conceptual::EntityBuilder
+      include Singleton
+      def initialize
+        int(:age)
+        string(:name)
+        date(:birthday)
+        datetime(:created_at)
+      end
+    end
+
+    subject(:player) { Player.instance }
+
+    describe "attributes" do
+      subject { player.attributes }
+
+      it "returns all attributes via `attributes`" do
+        expect(subject.size).to eq(4)
+      end
+
+      it "contains `age` int field as first element" do
+        expect(subject[0]).to eq(Conceptual::IntAttribute.new(:age))
+      end
+
+      it "contains `name` string field as second element" do
+        expect(subject[1]).to eq(Conceptual::StringAttribute.new(:name))
+      end
+
+      it "contains `birthday` date field as second element" do
+        expect(subject[2]).to eq(Conceptual::DateAttribute.new(:birthday))
+      end
+
+      it "contains `created_at` datetime field as second element" do
+        expect(subject[3]).to eq(Conceptual::DateTimeAttribute.new(:created_at))
+      end
+
+    end
+
+  end
+
 end
