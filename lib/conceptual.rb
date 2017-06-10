04c241820bc814ae8158cfddff9a7fb33d38a715
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-06-10 22:50:55 +0900

    WIP ddl

--- lib/conceptual.rb
+++ lib/conceptual.rb
@@ -2,6 +2,7 @@ require "conceptual/ext/string"
 require "conceptual/attributes"
 require "conceptual/entity_builder"
 require "conceptual/version"
+require "conceptual/sql/ddl"
 
 module Conceptual
 end
--- lib/conceptual/sql/ddl.rb
+++ lib/conceptual/sql/ddl.rb
@@ -2,32 +2,35 @@ require 'conceptual/attributes'
 
 module Conceptual
 
-  module Ddl
+  module Sql
 
-    def self.build(entity)
+    def self.ddl(entity)
       ddl = "CREATE TABLE `#{entity.name}` ("
 
       separator = ''
       entity.attributes.map { |attr|
         ddl += separator
-        ddl += '\n'
+        ddl += "\n"
         ddl += '  '
-        ddl += attr.name
+        ddl += attr.name.to_s
         ddl += ' '
-        ddl += 'int'
+        ddl += datatype(attr)
+        separator = ','
       }
 
-      ddl + ')'
+      ddl + "\n)"
     end
 
     private
     def self.datatype(attr)
-      if attr is_a? Conceptual::IntAttribute
+      if attr.is_a? Conceptual::IntAttribute
         'INT(11)'
-      elsif attr is_a? Conceptual::StringAttribute
+      elsif attr.is_a?(Conceptual::StringAttribute)
         'VARCHAR(255)'
-      elsif attr is_a? Conceptual::DateAttribute
-        # TODO
+      elsif attr.is_a? Conceptual::DateAttribute
+        'DATE'
+      elsif attr.is_a? Conceptual::DateTimeAttribute
+        'DATETIME'
       end
     end
 
